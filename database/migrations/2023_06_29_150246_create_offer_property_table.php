fa9dd948c007511f1c07014aeba14d3f2ae9b1ed
davidecaputo <daca9621@gmail.com>
2023-06-29 17:19:22 +0200

    add offert propertiy migration

---
+++ database/migrations/2023_06_29_150246_create_offer_property_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('offer_property', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('offer_id');
+            $table->foreign('offer_id')->references('id')->on('offers')->cascadeOnDelete();
+
+            $table->foreignId('property_id');
+            $table->foreign('property_id')->references('id')->on('properties')->cascadeOnDelete();
+
+            $table->dateTime('started_at');
+            $table->dateTime('started_at');
+            $table->boolean('expired');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('offer_property');
+    }
+};
