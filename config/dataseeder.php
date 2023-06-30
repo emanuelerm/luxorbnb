41608ee7b99c0324a810e99d65a0f34854817766
emanuelerm <emanueler00@gmail.com>
2023-06-30 10:14:00 +0200

    dataseeder

---
+++ config/dataseeder.php
@@ -0,0 +1,42 @@
+<?php
+
+$property = [
+
+    'id' => 1,
+    'title' => 'Appartamento accogliente nel centro della città',
+    'rooms' => 2,
+    'beds' => 3,
+    'bathrooms' => 1,
+    'square_meters' => 80,
+    'address' => 'Via Roma, 123',
+    'latitude' => 41.9028,
+    'longitude' => 12.4964,
+    'visible' => true,
+    ];
+
+    $user = [
+        'id' => 1,
+        'email' => 'user@example.com',
+        'password' => 'password',
+        'name' => 'mioNome',
+        'lastname' => 'MioCognome',
+        'date_of_birth' => '1990-01-01',
+
+
+    ];
+
+    $message = [
+        'id' => 1,
+        'apartment_id' => 1,
+        'mitente_email' => 'user@example.com',
+        'message' => 'Mi interessa affittare l appartamento.',
+
+    ];
+
+    $offer = [
+        'name' => 'bronze',
+        'duration' => 24,
+        'price' => 2.99,
+        'start_date' => '2023-06-28 12:00:00',
+        'end_date' => '2023-06-29 12:00:00',
+    ];
