d527e4bb2073f591dc5286d995c256a45f623103
JorgeLuix <tec.jorge.29@gmai.com>
2023-06-30 10:10:43 +0200

    ImageSeeder and ServiceSeeder

--- database/seeders/ServiceSeeder.php
+++ database/seeders/ServiceSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Service;
 
 class ServiceSeeder extends Seeder
 {
@@ -14,6 +15,19 @@ class ServiceSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $services = [
+            'WiFi',
+            'Posto Macchina',
+            'Piscina',
+            'Portineria',
+            'Sauna',
+            'Vista Mare'
+        ];
+
+        foreach ($services as $service) {
+            $newService = new Service();
+            $newService->name = $service;
+            $newService->save();
+        }
     }
 }
