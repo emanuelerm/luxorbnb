646a0c90534f681cf9290f5780661857987e02a2
MatteoAgu01 <matteoaguiari012@gmail.com>
2023-06-30 12:28:34 +0200

    add link in dash board

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,26 +1,30 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <h2 class="fs-4 text-secondary my-4">
-        {{ __('Dashboard') }}
-    </h2>
-    <div class="row justify-content-center">
-        <div class="col">
-            <div class="card">
-                <div class="card-header">{{ __('User Dashboard') }}</div>
+	<div class="container">
+		<h2 class="fs-4 text-secondary my-4">
+			{{ __('Dashboard') }}
+		</h2>
+		<div class="row justify-content-center">
+			<div class="col">
+				<div class="card">
+					<div class="card-header">{{ __('User Dashboard') }}</div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                    <div class="alert alert-success" role="alert">
-                        {{ session('status') }}
-                    </div>
-                    @endif
+					<div class="card-body">
+						<div>
+							<a class="fw-bold text-dark fs-5" href="{{ route('admin.properties.create') }}">Agiungi una proprità</a>
+						</div>
 
-                    {{ __('You are logged in!') }}
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
+						@if (session('status'))
+							<div class="alert alert-success" role="alert">
+								{{ session('status') }}
+							</div>
+						@endif
+
+						{{ __('You are logged in!') }}
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
 @endsection
