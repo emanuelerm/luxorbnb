57821f657b870f66bb22ff9cc26235c1a5d6f870
emanuelerm <emanueler00@gmail.com>
2023-06-30 10:04:26 +0200

    rote fixed

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,6 @@ Route::get('/', function () {
 });
 Route::middleware(['auth', 'verified'])->name('admin.')->prefix('admin')->group(function () {
     Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
-    Route::resource('/characters', CharacterController::class);
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
