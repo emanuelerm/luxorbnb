558b97447aa8388ba01f93d094103c878d943a52
JorgeLuix <tec.jorge.29@gmai.com>
2023-06-30 18:08:57 +0200

    add modal-delete

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,3 +4,36 @@ import * as bootstrap from 'bootstrap';
 import.meta.glob([
     '../img/**'
 ])
+
+/** MODAL-DELETE****/
+const el = document.getElementById("wrapper");
+const toggleButton = document.getElementById("menu-toggle");
+
+toggleButton.addEventListener('click', function() {
+    el.classList.toggle("toggled");
+});
+
+// MOdal-delete
+const deleteSubmitButtons = document.querySelectorAll('.delete-button');
+
+deleteSubmitButtons.forEach((button) => {
+    button.addEventListener('click', (event) => {
+        event.preventDefault();
+
+        const dataTitle = button.getAttribute('data-item-title');
+
+        const modal = document.getElementById('deleteModal');
+
+        const bootstrapModal = new bootstrap.Modal(modal);
+        bootstrapModal.show();
+
+        const modalItemTitle = modal.querySelector('#modal-item-title');
+        modalItemTitle.textContent = dataTitle;
+
+        const buttonDelete = modal.querySelector('button.btn-primary');
+
+        buttonDelete.addEventListener('click', () => {
+            button.parentElement.submit();
+        });
+    });
+});
