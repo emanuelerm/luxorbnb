0f8ae08a07e005cf19e74fe595fe4352325cbe3e
Christian01881 <chrispamo01881@gmail.com>
2023-07-01 19:49:18 +0200

    fix login style

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -78,9 +78,9 @@
 		<form method="POST" action="{{ route('register') }}">
             @csrf
 
-			<h1>Create Account</h1>
+			<h1 class="mb-4">Crea un Account</h1>
             <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
-			<input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name"
+			<input id="name" type="text" class="form-control input-style @error('name') is-invalid @enderror" name="name"
 				value="{{ old('name') }}" required autocomplete="name" autofocus>
 			@error('name')
 			    <span class="invalid-feedback" role="alert">
@@ -88,7 +88,7 @@
 			    </span>
 			@enderror
             <label for="surname" class="col-md-4 col-form-label text-md-right">{{ __('Surname') }}</label>
-            <input id="surname" type="text" class="form-control @error('surname') is-invalid @enderror" name="surname"
+            <input id="surname" type="text" class="form-control input-style @error('surname') is-invalid @enderror" name="surname"
 				value="{{ old('surname') }}" required autocomplete="surname" autofocus>
 			@error('surname')
 				<span class="invalid-feedback" role="alert">
@@ -96,7 +96,7 @@
 				</span>
 			@enderror
             <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-			<input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email"
+			<input id="email" type="email" class="form-control input-style @error('email') is-invalid @enderror" name="email"
 				value="{{ old('email') }}" required autocomplete="email">
 			@error('email')
 			    <span class="invalid-feedback" role="alert">
@@ -104,7 +104,7 @@
 				</span>
 			@enderror
             <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-			<input id="password" type="password" class="form-control @error('password') is-invalid @enderror"
+			<input id="password" type="password" class="form-control input-style @error('password') is-invalid @enderror"
 				name="password" required autocomplete="new-password">
 			@error('password')
 				<span class="invalid-feedback" role="alert">
@@ -112,38 +112,38 @@
 				</span>
 			@enderror
             <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
-            <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required
+            <input id="password-confirm" type="password" class="form-control input-style" name="password_confirmation" required
 				autocomplete="new-password">
                 <label for="date_of_birth" class="col-md-4 col-form-label text-md-right">{{ __('Date_of_birth') }}</label>
-            <input id="date_of_birth" type="date" class="form-control @error('date_of_birth') is-invalid @enderror" name="date_of_birth"
+            <input id="date_of_birth" type="date" class="form-control mb-4 input-style @error('date_of_birth') is-invalid @enderror" name="date_of_birth"
 			    value="{{ old('date_of_birth') }}" required autocomplete="date_of_birth" autofocus>
 			@error('date_of_birth')
 				<span class="invalid-feedback" role="alert">
 					<strong>{{ $message }}</strong>
 				</span>
 			@enderror
-			<button type="submit">{{ __('Register') }}</button>
+			<button type="submit" class="mb-4">{{ __('Register') }}</button>
 		</form>
 	</div>
 	<div class="form-container sign-in-container">
 		<form method="POST" action="{{ route('login') }}">
             @csrf
-			<h1>Sign in</h1>
+			<h1 class="mb-5">Accedi</h1>
             <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-			<input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+			<input id="email" type="email" class="form-control input-style @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
             @error('email')
                 <span class="invalid-feedback" role="alert">
                     <strong>{{ $message }}</strong>
                 </span>
             @enderror
             <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-			<input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+			<input id="password" type="password" class="form-control input-style mb-5 @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
             @error('password')
                 <span class="invalid-feedback" role="alert">
                     <strong>{{ $message }}</strong>
                 </span>
             @enderror
-			<button type="submit">{{ __('Login') }}</button>
+			<button type="submit mt-5">{{ __('Login') }}</button>
             @if (Route::has('password.request'))
                 <a class="btn btn-link" href="{{ route('password.request') }}">
                     {{ __('Forgot Your Password?') }}
@@ -154,14 +154,14 @@
 	<div class="overlay-container">
 		<div class="overlay">
 			<div class="overlay-panel overlay-left">
-				<h1>Welcome Back!</h1>
-				<p>To keep connected with us please login with your personal info</p>
-				<button class="ghost" id="signIn">Sign In</button>
+				<h1>Benvenuto!</h1>
+				<p>Inserisci le tue credenziali per registrarti e accedere al tuo spazio privato</p>
+				<button class="ghost" id="signIn">Login</button>
 			</div>
 			<div class="overlay-panel overlay-right">
-				<h1>Hello, Friend!</h1>
-				<p>Enter your personal details and start journey with us</p>
-				<button class="ghost" id="signUp">Sign Up</button>
+				<h1>Bentornato!</h1>
+				<p>Inserisci la tua mail e la password per accedere al tuo spazio privato</p>
+				<button class="ghost" id="signUp">Register</button>
 			</div>
 		</div>
 	</div>
