65e9f8084606b172153ab02cebf0a05545669ef9
Christian01881 <chrispamo01881@gmail.com>
2023-07-01 15:45:11 +0200

    half work

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,7 +1,8 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container mt-4">
+@vite(['resources/scss/partials/login.scss', 'resources/js/login.js'])
+{{-- <div class="container mt-4">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
@@ -69,5 +70,81 @@
             </div>
         </div>
     </div>
+</div> --}}
+
+<h2>Weekly Coding Challenge #1: Sign in/up Form</h2>
+<div class="container-1" id="container">
+	<div class="form-container sign-up-container">
+		<form action="#">
+			<h1>Create Account</h1>
+            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
+			<input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name"
+				value="{{ old('name') }}" required autocomplete="name" autofocus>
+			@error('name')
+			    <span class="invalid-feedback" role="alert">
+				    <strong>{{ $message }}</strong>
+			    </span>
+			@enderror
+            <label for="surname" class="col-md-4 col-form-label text-md-right">{{ __('Surname') }}</label>
+            <input id="surname" type="text" class="form-control @error('surname') is-invalid @enderror" name="surname"
+				value="{{ old('surname') }}" required autocomplete="surname" autofocus>
+			@error('surname')
+				<span class="invalid-feedback" role="alert">
+					<strong>{{ $message }}</strong>
+				</span>
+			@enderror
+            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+			<input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email"
+				value="{{ old('email') }}" required autocomplete="email">
+			@error('email')
+			    <span class="invalid-feedback" role="alert">
+					<strong>{{ $message }}</strong>
+				</span>
+			@enderror
+			<input id="password" type="password" class="form-control @error('password') is-invalid @enderror"
+				name="password" required autocomplete="new-password">
+			@error('password')
+				<span class="invalid-feedback" role="alert">
+					<strong>{{ $message }}</strong>
+				</span>
+			@enderror
+			<button>Sign Up</button>
+		</form>
+	</div>
+	<div class="form-container sign-in-container">
+		<form action="#">
+			<h1>Sign in</h1>
+			<div class="social-container">
+				<a href="#" class="social"><i class="fab fa-facebook-f"></i></a>
+				<a href="#" class="social"><i class="fab fa-google-plus-g"></i></a>
+				<a href="#" class="social"><i class="fab fa-linkedin-in"></i></a>
+			</div>
+			<span>or use your account</span>
+			<input type="email" placeholder="Email" />
+			<input type="password" placeholder="Password" />
+			<a href="#">Forgot your password?</a>
+			<button>Sign In</button>
+		</form>
+	</div>
+	<div class="overlay-container">
+		<div class="overlay">
+			<div class="overlay-panel overlay-left">
+				<h1>Welcome Back!</h1>
+				<p>To keep connected with us please login with your personal info</p>
+				<button class="ghost" id="signIn">Sign In</button>
+			</div>
+			<div class="overlay-panel overlay-right">
+				<h1>Hello, Friend!</h1>
+				<p>Enter your personal details and start journey with us</p>
+				<button class="ghost" id="signUp">Sign Up</button>
+			</div>
+		</div>
+	</div>
 </div>
+
+<footer>
+	<p>
+		Created by Group-5
+	</p>
+</footer>
 @endsection
