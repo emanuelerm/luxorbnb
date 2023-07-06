cd00f90ff55193e620f482da5a5aed6856b463e1
Christian01881 <chrispamo01881@gmail.com>
2023-07-06 12:53:50 +0200

    api created

---
+++ app/Http/Controllers/Api/PropertyController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Models\Property;
+use App\Models\Service;
+use App\Models\Image;
+use App\Http\Controllers\Controller;
+use GrahamCampbell\ResultType\Success;
+use Illuminate\Http\Request;
+
+class PropertyController extends Controller
+{
+    public function index()
+    {
+        $properties = Property::with('images', 'services')->paginate(5);
+        // $services = Service::all();
+        // $images = Image::all();
+        // $data = [
+        //     'properties' => $properties,
+        //     'services' => $services,
+        //     'images' => $images,
+        // ];
+        return response()->json([
+            "success" => true,
+            "results" => $properties
+        ]);
+    }
+
+    public function show($slug)
+    {
+        $property = Property::with('images', 'services')->where('slug', $slug)->first();
+        return response()->json([
+            "success" => true,
+            "results" => $property
+        ]);
+    }
+}
