a969d5505f7ee11b9c910d5cfeb0851a00f3ca6a
MatteoAgu01 <matteoaguiari012@gmail.com>
2023-07-07 15:24:48 +0200

    get longitude and latitude

--- app/Http/Controllers/Admin/PropertyController.php
+++ app/Http/Controllers/Admin/PropertyController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Admin;
 
+use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\Controller;
 use App\Models\Property;
@@ -13,202 +14,235 @@ use App\Http\Requests\UpdatePropertyRequest;
 use Illuminate\Support\Str;
 use PHPUnit\Framework\MockObject\ReturnValueNotConfiguredException;
 
+use function PHPUnit\Framework\returnSelf;
+
 class PropertyController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        $properties = Property::paginate(3);
-        $user = Auth::user();
-        return view('admin.properties.index', compact('properties', 'user'));
-    }
+	/**
+	 * Display a listing of the resource.
+	 *
+	 * @return \Illuminate\Http\Response
+	 */
+	public function index()
+	{
+		$properties = Property::paginate(3);
+		$user = Auth::user();
+		return view('admin.properties.index', compact('properties', 'user'));
+	}
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        $services = Service::all();
-        // $images = Image::all();
-        return view('admin.properties.create', compact('services'));
-    }
+	/**
+	 * Show the form for creating a new resource.
+	 *
+	 * @return \Illuminate\Http\Response
+	 */
+	public function create()
+	{
+		$services = Service::all();
+		// $images = Image::all();
+		return view('admin.properties.create', compact('services'));
+	}
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \App\Http\Requests\StorePropertyRequest  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(StorePropertyRequest $request)
-{
-    $user = Auth::user();
-    $form_data = $request->validated();
-    $newProperty = new Property();
-    $newProperty->user_id = $user->id;
-    $slug = $this->getSlug($form_data['title'], 'form_data->title');
-    $form_data['slug'] = $slug;
-    // dd($request);
-    $newProperty->fill($form_data);
-    $newProperty->save();
+	/**
+	 * Store a newly created resource in storage.
+	 *
+	 * @param  \App\Http\Requests\StorePropertyRequest  $request
+	 * @return \Illuminate\Http\Response
+	 */
+	public function store(StorePropertyRequest $request)
+	{
+		$user = Auth::user();
+		$form_data = $request->validated();
+		$newProperty = new Property();
+		$newProperty->user_id = $user->id;
+		$slug = $this->getSlug($form_data['title'], 'form_data->title');
+		$form_data['slug'] = $slug;
 
-    if ($request->has('services')) {
-        $newProperty->services()->sync($request->services);
-    }
-    // dd($request);
-    if ($request->hasFile('images')) {
-        foreach ($request->file('images') as $image) {
-            $image_path = Storage::put('uploads', $image);
-            // dd($image_path);
+		//GET LONGITUDE AND LATITUDE
+		$key = '6Zdz4adkb3YzaPURg8Zg71KMzMez217G'; //inserite la vostra chiave
+		$addressApi = $form_data['address'];
+		$addressUrl = Str::of($addressApi)->slug("%20");
+		$url = 'https://api.tomtom.com/search/2/geocode/' . $addressUrl . '.json?storeResult=false&view=Unified&key=' . $key;
+		$response = Http::get($url);
+		if ($response->successful()) {
+			$data = $response->json();
 
-            // $image_path = Str::replace('uploads', 'public/storage/uploads', $image_path);
+			if (!empty($data['results'])) {
+				$results = $data['results'];
 
-            $newProperty->images()->create([
-                'property_id' => $newProperty->id,
-                'path' => $image_path
-            ]);
-        }
-    }
+				// Assumi che il primo risultato sia quello desiderato
+				$firstResult = $results[0];
 
-    return redirect()->route('admin.properties.index');
-}
+				// Verifica se latitudine e longitudine sono presenti
+				if (isset($firstResult['position']['lat']) && isset($firstResult['position']['lon'])) {
+					$latitudine = $firstResult['position']['lat'];
+					$longitudine = $firstResult['position']['lon'];
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Property  $property
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Property $property)
-    {
+					// Salva i dati nella tua tabella del database
+					$form_data['latitude'] = $latitudine;
+					$form_data['longitude'] = $longitudine;
+				}
+			}
+		}
+		//dd($form_data);
+		$newProperty->fill($form_data);
+		$newProperty->save();
 
-        $services = Service::all();
-        $images = Image::where('property_id', $property->id)->get();
-        // $images = $property->images;
-        return view('admin.properties.show', compact('property', 'services','images'));
-    }
+		if ($request->has('services')) {
+			$newProperty->services()->sync($request->services);
+		}
+		// dd($request);
+		if ($request->hasFile('images')) {
+			foreach ($request->file('images') as $image) {
+				$image_path = Storage::put('uploads', $image);
+				// dd($image_path);
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Property  $property
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Property $property)
-    {
-        // if (!Auth::user()->is_admin && $property->user_id !== Auth::id()) {
-        //     abort(403);
-        // }
-        $services = Service::all();
-        return view('admin.properties.edit', compact('property', 'services'));
-    }
+				// $image_path = Str::replace('uploads', 'public/storage/uploads', $image_path);
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \App\Http\Requests\UpdatePropertyRequest  $request
-     * @param  \App\Models\Property  $property
-     * @return \Illuminate\Http\Response
-     */
-    public function update(UpdatePropertyRequest $request, Property $property)
-    {
-        $user = Auth::user();
-        $form_data = $request->validated();
-        // dd($form_data);
-        // Aggiornare i dati della proprietà esistente invece di creare una nuova proprietà
-        $property->user_id = $user->id;
-        $property->fill($form_data);
-        $property->save();
+				$newProperty->images()->create([
+					'property_id' => $newProperty->id,
+					'path' => $image_path
+				]);
+			}
+		}
 
-        if ($request->has('services')) {
-            $property->services()->sync($request->services);
-        } else {
-            $property->services()->sync([]);
-        }
+		return redirect()->route('admin.properties.index');
+	}
 
-        if ($request->hasFile('images')) {
-            foreach ($request->file('images') as $image) {
-                $image_path = Storage::put('uploads', $image);
+	/**
+	 * Display the specified resource.
+	 *
+	 * @param  \App\Models\Property  $property
+	 * @return \Illuminate\Http\Response
+	 */
+	public function show(Property $property)
+	{
 
-                // $image_path = Str::replace('uploads', 'public/storage/uploads', $image_path);
+		$services = Service::all();
+		$images = Image::where('property_id', $property->id)->get();
+		// $images = $property->images;
+		return view('admin.properties.show', compact('property', 'services', 'images'));
+	}
 
-                $property->images()->create([
-                    'property_id' => $property->id,
-                    'path' => $image_path
-                ]);
-            }
-        }
+	/**
+	 * Show the form for editing the specified resource.
+	 *
+	 * @param  \App\Models\Property  $property
+	 * @return \Illuminate\Http\Response
+	 */
+	public function edit(Property $property)
+	{
+		// if (!Auth::user()->is_admin && $property->user_id !== Auth::id()) {
+		//     abort(403);
+		// }
+		$services = Service::all();
+		return view('admin.properties.edit', compact('property', 'services'));
+	}
 
-        $imagesToDelete = $request->input('images_to_delete');
-        if ($imagesToDelete) {
-            foreach ($imagesToDelete as $imageToDelete) {
-                $jsonImage = json_decode($imageToDelete, true);
+	/**
+	 * Update the specified resource in storage.
+	 *
+	 * @param  \App\Http\Requests\UpdatePropertyRequest  $request
+	 * @param  \App\Models\Property  $property
+	 * @return \Illuminate\Http\Response
+	 */
+	public function update(UpdatePropertyRequest $request, Property $property)
+	{
+		$user = Auth::user();
+		$form_data = $request->validated();
+		// dd($form_data);
+		// Aggiornare i dati della proprietà esistente invece di creare una nuova proprietà
+		$property->user_id = $user->id;
+		$property->fill($form_data);
+		$property->save();
 
-                $image = Image::findOrFail($jsonImage['id']);
-                Storage::delete($jsonImage['path']);
-                // Effettua le operazioni necessarie per eliminare l'immagine,
-                // come eliminare il record dal database e il file fisico dell'immagine.
-                $image->delete();
-            }
-        }
+		if ($request->has('services')) {
+			$property->services()->sync($request->services);
+		} else {
+			$property->services()->sync([]);
+		}
 
+		if ($request->hasFile('images')) {
+			foreach ($request->file('images') as $image) {
+				$image_path = Storage::put('uploads', $image);
 
-        return redirect()->route('admin.properties.show', compact('property'))->with('message', "{$property->title} è stato aggiornato correttamente");
-    }
+				// $image_path = Str::replace('uploads', 'public/storage/uploads', $image_path);
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Property  $property
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Property $property)
-    {
-        $property->delete();
-        return redirect()->route('admin.properties.index')->with('message', "{$property->title} è stato cancellato correttamente");
-    }
+				$property->images()->create([
+					'property_id' => $property->id,
+					'path' => $image_path
+				]);
+			}
+		}
 
-    private function getSlug($title)
-    {
-        $slug = Str::of($title)->slug("-");
-        $count = 1;
+		$imagesToDelete = $request->input('images_to_delete');
+		if ($imagesToDelete) {
+			foreach ($imagesToDelete as $imageToDelete) {
+				$jsonImage = json_decode($imageToDelete, true);
 
-        // Prendi il primo post il cui slug è uguale a $slug
-        // se è presente allora genero un nuovo slug aggiungendo -$count
-        while( Property::where("slug", $slug)->first() ) {
-            $slug = Str::of($title)->slug("-") . "-{$count}";
-            $count++;
-        }
-
-        return $slug;
-    }
+				$image = Image::findOrFail($jsonImage['id']);
+				Storage::delete($jsonImage['path']);
+				// Effettua le operazioni necessarie per eliminare l'immagine,
+				// come eliminare il record dal database e il file fisico dell'immagine.
+				$image->delete();
+			}
+		}
 
 
-    // private function getSlug($property)
-    // {
-    //     $slug = Str::slug($property->title);
-    //     $count = 2;
+		return redirect()->route('admin.properties.show', compact('property'))->with('message', "{$property->title} è stato aggiornato correttamente");
+	}
 
-    //     // Prendi il primo post il cui slug è uguale a $slug
-    //     // se è presente allora genero un nuovo slug aggiungendo -$count
-    //     while (static::where("slug", $slug)->exist()) {
-    //         $slug = Str::slug($property->title) . "-{$count}";
-    //         $count++;
-    //     }
+	/**
+	 * Remove the specified resource from storage.
+	 *
+	 * @param  \App\Models\Property  $property
+	 * @return \Illuminate\Http\Response
+	 */
+	public function destroy(Property $property)
+	{
+		$property->delete();
+		return redirect()->route('admin.properties.index')->with('message', "{$property->title} è stato cancellato correttamente");
+	}
 
-    //     $property->slug = $slug;
-    // }
-    // protected static function boot()
-    // {
-    //     parent::boot();
+	private function getSlug($title)
+	{
+		$slug = Str::of($title)->slug("-");
+		$count = 1;
 
-    //     static::saving(function ($model) {
-    //         $model->getSlug();
-    //     });
-    // }
+		// Prendi il primo post il cui slug è uguale a $slug
+		// se è presente allora genero un nuovo slug aggiungendo -$count
+		while (Property::where("slug", $slug)->first()) {
+			$slug = Str::of($title)->slug("-") . "-{$count}";
+			$count++;
+		}
+
+		return $slug;
+	}
+
+
+	private function getGeoCod($address)
+	{
+	}
+
+	// private function getSlug($property)
+	// {
+	//     $slug = Str::slug($property->title);
+	//     $count = 2;
+
+	//     // Prendi il primo post il cui slug è uguale a $slug
+	//     // se è presente allora genero un nuovo slug aggiungendo -$count
+	//     while (static::where("slug", $slug)->exist()) {
+	//         $slug = Str::slug($property->title) . "-{$count}";
+	//         $count++;
+	//     }
+
+	//     $property->slug = $slug;
+	// }
+	// protected static function boot()
+	// {
+	//     parent::boot();
+
+	//     static::saving(function ($model) {
+	//         $model->getSlug();
+	//     });
+	// }
 }
