6f1c219cfb32515e7d22638c58fd23ccf48bd320
Christian01881 <chrispamo01881@gmail.com>
2023-07-15 12:55:48 +0200

    per matte2

--- resources/views/admin/properties/show.blade.php
+++ resources/views/admin/properties/show.blade.php
@@ -65,19 +65,13 @@
                 <div class="container pb-5">
                     <div class="tab_container">
                         <input id="tab1" type="radio" name="tabs" checked>
-                        <label for="tab1"><i class="fa fa-code"></i><span>Code</span></label>
+                        <label for="tab1"><i class="fa fa-code"></i><span>Week</span></label>
 
                         <input id="tab2" type="radio" name="tabs">
-                        <label for="tab2"><i class="fa fa-pencil-square-o"></i><span>About</span></label>
+                        <label for="tab2"><i class="fa fa-pencil-square-o"></i><span>Month</span></label>
 
                         <input id="tab3" type="radio" name="tabs">
-                        <label for="tab3"><i class="fa fa-bar-chart-o"></i><span>Services</span></label>
-
-                        <input id="tab4" type="radio" name="tabs">
-                        <label for="tab4"><i class="fa fa-folder-open-o"></i><span>Portfolio</span></label>
-
-                        <input id="tab5" type="radio" name="tabs">
-                        <label for="tab5"><i class="fa fa-envelope-o"></i><span>Contact</span></label>
+                        <label for="tab3"><i class="fa fa-bar-chart-o"></i><span>Year</span></label>
 
                         <section id="content1" class="tab-content">
                             <h3>Queste sono le statistiche dell'ultima settimana</h3>
@@ -105,34 +99,6 @@
                                 consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
                                 cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
                                 proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                                consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-                                cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-                                proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                        </section>
-
-                        <section id="content4" class="tab-content">
-                            <h3>Headline 4</h3>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                                tempor incididunt ut labore et dolore magna aliqua.</p>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                                consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-                                cillum dolore eu fugiat nulla pariatur.</p>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                                consequat.</p>
-                        </section>
-
-                        <section id="content5" class="tab-content">
-                            <h3>Headline 5</h3>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo.</p>
                             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
                                 tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
                                 quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
